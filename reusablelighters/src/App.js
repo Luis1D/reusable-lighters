bc9ffa86588d8eb6aa11f1c78c883f790db74553
Luis De Anda <luisdeanda916@outlook.com>
2019-12-10 13:30:55 -0800

    Footer component added

--- reusablelighters/src/App.js
+++ reusablelighters/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Banner from './components/Banner';
 import Featured from './components/Featured';
+import Footer from './components/Footer';
 import './styles/app.scss';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Banner />
       <Featured />
+      <Footer />
     </div>
   );
 }
