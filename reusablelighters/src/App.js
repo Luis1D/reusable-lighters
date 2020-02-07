1fa91708e5e6a41d84cf71d66d653d3ced3c10e1
Luis De Anda <luisdeanda916@outlook.com>
2020-02-06 21:18:35 -0800

    menu button added to header component

--- reusablelighters/src/App.js
+++ reusablelighters/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import Banner from './components/Banner';
-import Featured from './components/Featured';
 import Footer from './components/Footer';
 import productList from './data/productList';
 import './styles/app.scss';
@@ -14,19 +12,17 @@ function App() {
       <div className="product-list">
         {
           productList.map(product => {
-            return <a href={ product.href } target="_blank" key={ product.id }>
-            <div className="product-container">
+            return <div className="product-container" key={ product.id }>
               <div>          
-                  <img src={ product.src1 } />
+                  <img src={ product.src1 } alt="product" />
                 <img src={ product.src2 } alt="amazon ad" className="empty-img" />
               </div>
               <h2>{ product.title }</h2>
               <h3>${ product.price }</h3>
-              <a href={ product.href } target="_blank" className="btn-link">
+              <a href={ product.href } target="_blank" rel="noopener noreferrer" className="btn-link">
                 <button>View Product</button>
               </a>
             </div>
-            </a>
           })
         }
       </div>
--- reusablelighters/src/components/Header.js
+++ reusablelighters/src/components/Header.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header = () => {
+    const [menu, setMenu] = useState(false);
+    const menuTrigger = () => {
+        setMenu(!menu);
+    }
     return (
         <div className="Header">
-            <img
-                src={ require('../assets/bolt.svg') }
-                alt="logo"
-            />
-            {/* Icons made by <a href="https://www.flaticon.com/authors/xnimrodx" title="xnimrodx">xnimrodx</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */}
-            <h1>Reusable Lighters</h1>
+            <div className="main-header">
+                <div className="container">
+                    <img
+                        src={ require('../assets/bolt.svg') }
+                        alt="logo"
+                        className="logo"
+                    />
+                    {/* Icons made by <a href="https://www.flaticon.com/authors/xnimrodx" title="xnimrodx">xnimrodx</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */}
+                    <h1 className="logo-title">Reusable Lighters</h1>
+                </div>
+                <div className="container">
+                    <img
+                        src={ require('../assets/menu.svg') }
+                        alt="menu"
+                        className="menu-icon"
+                        onClick={ menuTrigger }
+                    />
+                </div>
+            </div>
+            <div className="menu-container">
+                <div className={ menu ? "menu-active" : "menu-hidden" }>
+                <h1 onClick={ menuTrigger }>X</h1>
+                    <ul>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
