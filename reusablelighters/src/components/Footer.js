2e1e6dd97707a7ee98c415090aa3c55180bf10bb
Luis De Anda <luisdeanda916@outlook.com>
2020-02-06 21:33:36 -0800

    atributions added

--- reusablelighters/src/components/Footer.js
+++ reusablelighters/src/components/Footer.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="Footer">
+            <span>Icons provided by <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></span>
+            <span>© 2020 Reusablelighters.com</span>
         </div>
     )
 }
